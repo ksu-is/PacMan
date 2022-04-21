0a3d866d650467457bf10f79fdce88371d114f28
Udochi Nwachukwu <87030936+UdochiNwachu@users.noreply.github.com>
2022-04-20 20:33:07 -0400

    Update app_class.py

--- app_class.py
+++ app_class.py
@@ -51,3 +51,35 @@ class App:
                         pygame.draw.rect(self.background, BLACK, (xidx * self.cell_width, yidx * self.cell_height, self.cell_width, self.cell_height))
     
     ############### Video 3 ############################################
+    
+    def make_enemies(self):
+        for idx, pos in enumerate(self.e_pos):
+            self.enemies.append(Enemy(self, vec(pos), idx))
+
+    def draw_grid(self):
+        for x in range(WIDTH//self.cell_width):
+            pygame.draw.line(self.background, GREY, (x * self.cell_width, 0), (x * self.cell_width, HEIGHT))
+        for x in range(HEIGHT//self.cell_height):
+            pygame.draw.line(self.background, GREY, (0, x * self.cell_height), (WIDTH, x * self.cell_height))
+        #for coin in self.coins:
+            #pygame.draw.rect(self.background, (167, 179, 34), (coin.x * self.cell_width, coin.y * self.cell_height, self.cell_width, self.cell_height))
+
+    def reset(self):
+        self.player.lives = 3
+        self.player.current_score = 0
+        self.player.grid_pos = vec(self.player.starting_pos)
+        self.player.pix_pos = self.player.get_pix_pos()
+        self.player.direction *= 0
+        for enemy in self.enemies:
+            enemy.grid_pos = vec(enemy.starting_pos)
+            enemy.pix_pos = enemy.get_pix_pos()
+            enemy.direction *= 0
+        self.coins = []
+        with open("C:\\Users\\Udochi Nwachukwu\\OneDrive\\Documents\\PacmanGame\\walls.txt", 'r') as file:
+            for yidx, line in enumerate(file):
+                for xidx, char in enumerate(line):
+                    if char == "C":
+                        self.coins.append(vec(xidx, yidx))
+        self.state = "playing"
+
+  ############################ Video 5 ###################################
