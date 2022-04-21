02be05c91583465e168e6d24cb6ab7c0910bef94
Udochi Nwachukwu <87030936+UdochiNwachu@users.noreply.github.com>
2022-04-21 13:26:55 -0400

    Update player_class.py

--- player_class.py
+++ player_class.py
@@ -31,3 +31,45 @@ class Player:
             self.eat_coin()
             
  ####################### Video 6 ##################################
+
+def draw(self):
+        pygame.draw.circle(self.app.screen, PLAYER_COLOR, (int(self.pix_pos.x), self.pix_pos.y), self.app.cell_width//2 - 2)
+
+        # Drawing player lives
+        for x in range(self.lives):
+            pygame.draw.circle(self.app.screen, PLAYER_COLOR, (30 + 20 * x, HEIGHT - 15), 7)
+
+
+
+        # Drawing the grid pos rect
+        #pygame.draw.rect(self.app.screen, RED, (self.grid_pos[0] * self.app.cell_width + TOP_BOTTOM_BUFFER//2, self.grid_pos[1] * self.app.cell_height + TOP_BOTTOM_BUFFER//2, self.app.cell_width, self.app.cell_height), 1)
+
+    def on_coin(self):
+        if self.grid_pos in self.app.coins:
+            if (self.pix_pos.x + TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+                if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                    return True
+            if (self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+                if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                    return True
+        return False
+
+    def eat_coin(self):
+        self.app.coins.remove(self.grid_pos)
+        self.current_score += 1
+
+    def move(self, direction):
+        self.stored_direction = direction
+
+    def get_pix_pos(self):
+        return vec((self.grid_pos.x * self.app.cell_width) + TOP_BOTTOM_BUFFER//2 + self.app.cell_width//2, (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_BUFFER//2 + self.app.cell_height//2)
+        print(self.grid_pos, self.pix_pos)
+
+    def time_to_move(self):
+        if int(self.pix_pos.x + TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
+                return True
+        if int(self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
+                return True
+  ################################### Video 7 ##############################################
