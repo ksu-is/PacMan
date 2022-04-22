fc320a2a5fde56c60f65b0daee6ec5cc29e2d71c
Udochi Nwachukwu <87030936+UdochiNwachu@users.noreply.github.com>
2022-04-22 10:14:27 -0400

    Update app_class.py

--- app_class.py
+++ app_class.py
@@ -154,3 +154,26 @@ def playing_draw(self):
         for coin in self.coins:
             pygame.draw.circle(self.screen, (124, 123, 7), (int(coin.x * self.cell_width) + self.cell_width//2 + TOP_BOTTOM_BUFFER//2, int(coin.y * self.cell_height) + self.cell_height//2 + TOP_BOTTOM_BUFFER//2), 5)
 ################################### Video 12, 13, and 14 ###########################################
+
+######################### GAME OVER FUNCTIONS ###########################
+    
+    def game_over_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.reset()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running = False
+
+    def game_over_update(self):
+        pass
+
+    def game_over_draw(self):
+        self.screen.fill(BLACK)
+        quit_text = "Press the escape button to QUIT"
+        again_text = "Press SPACE bar to PLAY AGAIN"
+        self.draw_text("GAME OVER", self.screen, [WIDTH//2, 100], 52, RED, "arial", centered = True)
+        self.draw_text(again_text, self.screen, [WIDTH//2, HEIGHT//2], 36, (190, 190, 190), "arial", centered = True)
+        self.draw_text(quit_text, self.screen, [WIDTH//2, HEIGHT//1.5], 36, (190, 190, 190), "arial", centered = True)
+        pygame.display.update()
